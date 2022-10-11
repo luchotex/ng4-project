95a65e1a148c5a7a3b666ea7d9a654c4a2b85dcf
Luis Kupferberg Ruiz <luis.kupferberg@bairesdev.com>
2022-10-11 08:20:32 -0400

    -- Added functionality to add ingredients to shopping list. -- Moved the shoppingList providing to global app using.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ShoppingListService} from "./shopping-list/shopping-list.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ShoppingListService]
 })
 export class AppComponent {
   title = 'ng4-project';
   loadedFeature = 'recipe';
 
-  onNavigate(feature: string){
+  onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
 }
