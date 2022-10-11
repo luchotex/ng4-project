8356ef3b2566947f98a28cd9f6905707729d2304
Luis Kupferberg Ruiz <luis.kupferberg@bairesdev.com>
2022-10-11 07:21:26 -0400

    -- Created recipeSelected emitter to have this value in all components. -- Removed all outputs to emit the selected recipe.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -12,13 +12,14 @@ export class RecipesComponent implements OnInit {
 
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {
+  }
 
   ngOnInit(): void {
+    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
+      this.selectedRecipe = recipe;
+    });
   }
 
-  onSelectedRecipe(recipe: Recipe) {
-    this.selectedRecipe = recipe;
-  }
 
 }
